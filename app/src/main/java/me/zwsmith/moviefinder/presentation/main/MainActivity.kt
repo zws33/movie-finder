fba1bad4a5ed658395f3286cf68a076494c2f6d0
Zachary Smith <zach.smith33@gmail.com>
2018-08-25 17:45:03 -0400

    Initial commit

---
+++ app/src/main/java/me/zwsmith/moviefinder/presentation/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.zwsmith.moviefinder.presentation.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import me.zwsmith.moviefinder.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
