10a7e2c09ec34b68e03148fb5187838196efa081
Zachary Smith <zach.smith33@gmail.com>
2018-08-27 13:49:46 -0400

    Create movie details fragment and layout

---
+++ app/src/main/java/me/zwsmith/moviefinder/presentation/movieDetails/MovieDetailsFragment.kt
@@ -0,0 +1,18 @@
+package me.zwsmith.moviefinder.presentation.movieDetails
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.zwsmith.moviefinder.R
+
+class MovieDetailsFragment : Fragment() {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    }
+}
