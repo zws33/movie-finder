5e0d95ae497f092ba01aaa82d66210db0e5a7d00
Zachary Smith <zach.smith33@gmail.com>
2018-09-01 15:54:17 -0400

    Update logic in endless recyclerview on scroll listener.

--- app/src/main/java/me/zwsmith/moviefinder/presentation/common/EndlessRecyclerViewOnScrollListener.kt
+++ app/src/main/java/me/zwsmith/moviefinder/presentation/common/EndlessRecyclerViewOnScrollListener.kt
@@ -2,26 +2,42 @@ package me.zwsmith.moviefinder.presentation.common
 
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 
 
 abstract class EndlessRecyclerOnScrollListener : RecyclerView.OnScrollListener() {
+    private var previousTotal = 0
+    private var loading = true
+    private val visibleThreshold = 10
+    private var lastVisibleItem = 0
+    private var visibleItemCount = 0
+    private var totalItemCount = 0
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
 
-        if (dy > 0) {
-            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-            val visibleItemCount = layoutManager.childCount
-            val totalItemCount = recyclerView.adapter.itemCount
-            val pastVisiblesItems = layoutManager.findFirstVisibleItemPosition()
+        visibleItemCount = recyclerView.childCount
+        val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+        totalItemCount = layoutManager.itemCount
+        lastVisibleItem = layoutManager.findLastVisibleItemPosition()
 
-            if ((visibleItemCount + pastVisiblesItems) >= totalItemCount) {
-                Log.v("...", "Last Item Wow !")
+        if (dy > 0) {
+            if (loading) {
+                if (totalItemCount > previousTotal) {
+                    loading = false
+                    previousTotal = totalItemCount
+                }
+            }
+
+            if (!loading && (totalItemCount - visibleItemCount) <= (lastVisibleItem + visibleThreshold)) {
                 requestData()
+                loading = true
             }
         }
     }
 
     abstract fun requestData()
+
+    companion object {
+        private val TAG = EndlessRecyclerOnScrollListener::class.java.simpleName
+    }
 }
