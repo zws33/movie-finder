5e4251a1bec4a69909ba8b19398d07ec481c6775
Zachary Smith <zach.smith33@gmail.com>
2018-09-19 07:48:41 -0400

    Add define kodein models and initialize in application.

--- app/src/main/java/me/zwsmith/moviefinder/MovieFinderApplication.kt
+++ app/src/main/java/me/zwsmith/moviefinder/MovieFinderApplication.kt
@@ -1,20 +1,16 @@
 package me.zwsmith.moviefinder
 
 import android.app.Application
-import me.zwsmith.moviefinder.core.dependencyInjection.koin.*
-import org.koin.android.ext.android.startKoin
+import me.zwsmith.moviefinder.core.dependencyInjection.kodein.appModule
+import me.zwsmith.moviefinder.core.dependencyInjection.kodein.repositoryModule
+import me.zwsmith.moviefinder.core.dependencyInjection.kodein.serviceModule
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
 
-class MovieFinderApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        startKoin(this, koinModules)
+class MovieFinderApplication : Application(), KodeinAware {
+    override val kodein: Kodein = Kodein.lazy {
+        import(appModule)
+        import(serviceModule)
+        import(repositoryModule)
     }
-
-    private val koinModules = listOf(
-            appModule,
-            serviceModule,
-            repositoryModule,
-            interactorModule,
-            viewModelModule
-    )
 }
---
+++ app/src/main/java/me/zwsmith/moviefinder/core/dependencyInjection/kodein/AppModule.kt
@@ -0,0 +1,65 @@
+package me.zwsmith.moviefinder.core.dependencyInjection.kodein
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import me.zwsmith.moviefinder.core.common.ApiKeyInterceptor
+import me.zwsmith.moviefinder.core.repositories.MovieRepository
+import me.zwsmith.moviefinder.core.repositories.MovieRepositoryImpl
+import me.zwsmith.moviefinder.core.services.MovieService
+import okhttp3.OkHttpClient
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+val appModule = Kodein.Module(name = "AppModule") {
+    bind<Gson>() with singleton { provideGson() }
+    bind<ApiKeyInterceptor>() with singleton { provideApiKeyInterceptor() }
+    bind<OkHttpClient>() with singleton { provideOkHttpClient(apiKeyInterceptor = instance()) }
+    bind<Retrofit>() with singleton {
+        provideRetroFitInstance(gson = instance(), okHttpClient = instance())
+    }
+}
+
+val serviceModule = Kodein.Module(name = "ServiceModule") {
+    bind<MovieService>() with singleton { createService<MovieService>(instance()) }
+}
+
+val repositoryModule = Kodein.Module(name = "RepositoryModule") {
+    bind<MovieRepository>() with singleton { MovieRepositoryImpl(movieService = instance()) as MovieRepository }
+}
+
+private inline fun <reified T> createService(retrofit: Retrofit): T {
+    return retrofit.create(T::class.java)
+}
+
+private fun provideGson(): Gson {
+    return GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+}
+
+private fun provideApiKeyInterceptor(): ApiKeyInterceptor {
+    return ApiKeyInterceptor()
+}
+
+private fun provideOkHttpClient(apiKeyInterceptor: ApiKeyInterceptor): OkHttpClient {
+    return OkHttpClient.Builder()
+            .addInterceptor(apiKeyInterceptor)
+            .build()
+}
+
+private fun provideRetroFitInstance(gson: Gson, okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
+            .build()
+}
+
+private const val BASE_URL = "https://api.themoviedb.org/3/"
